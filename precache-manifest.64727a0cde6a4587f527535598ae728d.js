696a6582bb83050d98dc500e6956509554702526
Shubham Thakur <shubhamthakur.90@gmail.com>
2020-08-08 16:50:39 +0530

    Updates

---
+++ precache-manifest.64727a0cde6a4587f527535598ae728d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b733ab59743908780da0d535641d5c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5420384e7bdd4ba75c7d",
+    "url": "/monsters-rolodex/static/css/main.8a74437a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5420384e7bdd4ba75c7d",
+    "url": "/monsters-rolodex/static/js/main.76f1f324.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
